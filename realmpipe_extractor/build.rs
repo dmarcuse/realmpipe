cfb92227f20ba8db2d8aa8e3a386facf44d5d226
dmarcuse <dominic@marcuse.us>
2019-03-13 16:29:57 -0400

    Add travis config to test on linux, windows, osx

--- realmpipe_extractor/build.rs
+++ realmpipe_extractor/build.rs
@@ -1,74 +1,58 @@
-use git2::{Error as GitError, Repository};
+use git2::Repository;
 use std::convert::From;
-use std::fs::{read_dir, copy};
-use std::io::Error as IoError;
+use std::env;
+use std::fs::{copy, read_dir};
 use std::path::PathBuf;
-use std::process::{Command, ExitStatus};
-use std::env::{self, VarError as EnvError};
+use std::process::Command;
 
-#[derive(Debug)]
-enum BuildError {
-    IoError(IoError),
-    GitError(GitError),
-    CompileError(ExitStatus),
-    EnvError(EnvError),
-}
-
-impl From<IoError> for BuildError {
-    fn from(e: IoError) -> Self {
-        BuildError::IoError(e)
-    }
-}
-
-impl From<GitError> for BuildError {
-    fn from(e: GitError) -> Self {
-        BuildError::GitError(e)
-    }
-}
-
-impl From<EnvError> for BuildError {
-    fn from(e: EnvError) -> Self {
-        BuildError::EnvError(e)
-    }
-}
-
-fn main() -> Result<(), BuildError> {
+fn main() {
     // we don't need to recompile rabcdasm unless the build script changes
     println!("cargo:rerun-if-changed=build.rs");
 
     // create a temporary directory to build rabcdasm in
-    let dir = tempfile::tempdir()?;
+    let dir = tempfile::tempdir().expect("error creating temp dir");
     println!("Created temporary directory: {}", dir.as_ref().display());
 
     // clone rabcdasm
-    Repository::clone("https://github.com/CyberShadow/RABCDAsm.git", &dir)?;
+    Repository::clone("https://github.com/CyberShadow/RABCDAsm.git", &dir)
+        .expect("error cloning repo");
     println!("Successfully cloned repository");
 
     // build the project with dmd
     let exit_status = Command::new("dmd")
         .current_dir(&dir)
         .args(vec!["-run", "build_rabcdasm.d"])
-        .spawn()?
-        .wait()?;
+        .spawn()
+        .expect("error starting d compiler")
+        .wait()
+        .expect("error waiting for d compiler");
 
     if !exit_status.success() {
-        return Err(BuildError::CompileError(exit_status));
+        panic!("error compiling rabcdasm: {}", exit_status);
     }
 
     println!("Successfully compiled rabcdasm - directory contents:");
 
-    for file in read_dir(&dir)? {
-        println!("{}", file?.file_name().to_string_lossy());
+    for file in read_dir(&dir).unwrap() {
+        println!("{}", file.unwrap().file_name().to_string_lossy());
     }
 
     let src_dir = PathBuf::from(dir.as_ref());
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("error getting OUT_DIR variable"));
 
-    // the rabcdasm binaries we need
-    for name in &["abcexport", "rabcdasm", "swfbinexport"] {
-        copy(src_dir.join(PathBuf::from(name)), out_dir.join(PathBuf::from(name)))?;
+    // copy the rabcdasm binaries we need
+    for &name in &["abcexport", "rabcdasm", "swfbinexport"] {
+        let src = src_dir.join(if cfg!(not(windows)) {
+            PathBuf::from(name)
+        } else {
+            PathBuf::from(String::from(name) + ".exe")
+        });
+
+        copy(&src, out_dir.join(PathBuf::from(name))).expect(&format!(
+            "error copying {} binary from {}",
+            name,
+            src.display()
+        ));
         println!("Copied {} binary", name);
     }
-
-    Ok(())
 }
