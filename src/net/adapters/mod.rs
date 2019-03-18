c0d193caf1e0ebd85dcc0f824456fe5b43b09643
dmarcuse <dominic@marcuse.us>
2019-03-18 16:52:52 -0400

    Reimplement adapters using bytes crate

---
+++ src/net/adapters/mod.rs
@@ -0,0 +1,48 @@
+mod primitives;
+mod rle;
+
+use self::prelude::*;
+use failure::Fail;
+use std::convert::From;
+
+pub(self) mod prelude {
+    pub use super::{Error, NetworkAdapter, Result};
+    pub use bytes::{Buf, BufMut};
+}
+
+/// An error occuring when converting a type to or from big endian bytes
+#[derive(Debug, Fail)]
+pub enum Error {
+    /// Insufficient data left in the buffer to decode the given type
+    #[fail(
+        display = "Not enough data left in buffer: expected {}, got {}",
+        needed, remaining
+    )]
+    InsufficientData { remaining: usize, needed: usize },
+
+    /// A different type of error
+    #[fail(display = "{}", _0)]
+    Other(failure::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// A trait providing functionality for converting a type to or from buffers of
+/// bytes in big endian notation.
+pub trait NetworkAdapter: Sized + 'static {
+    /// Decode an instance from the given buffer. The amount of data remaining
+    /// in the buffer should be checked and AdapterError::InsufficientData
+    /// should be returned when appropriate.
+    fn get_be(bytes: &mut dyn Buf) -> Result<Self>;
+
+    /// Encode an instance to the given buffer. It may be assumed that the
+    /// buffer will be large enough to store the entire encoded sequence, so no
+    /// size checks are necessary.
+    fn put_be(self, bytes: &mut dyn BufMut) -> Result<()>;
+}
+
+impl From<failure::Error> for Error {
+    fn from(f: failure::Error) -> Self {
+        Error::Other(f)
+    }
+}
---
+++ src/net/adapters/rle.rs
@@ -0,0 +1,197 @@
+//! Implementations of `NetworkAdapter` for types that wrap collections and
+//! are preceded by a value indicating the number of items stored.
+
+use super::prelude::*;
+use failure::err_msg;
+use num::{FromPrimitive, ToPrimitive};
+use std::borrow::Borrow;
+use std::fmt::Display;
+use std::iter::{FromIterator, IntoIterator};
+use std::marker::PhantomData;
+use std::ops::Deref;
+
+/// A wrapper around a given collection (of type `C`) which can be converted
+/// to or from big endian bytes by prefixing the data with an integer (of type
+/// `S`).
+pub struct RLE<S, C> {
+    collection: C,
+    phantom: PhantomData<S>,
+}
+
+impl<S, C> RLE<S, C> {
+    /// Create a new run length encoded collection from the given collection
+    pub fn new(collection: C) -> Self {
+        Self {
+            collection: collection,
+            phantom: PhantomData::default(),
+        }
+    }
+
+    /// Unwrap the contained collection
+    pub fn unwrap(self) -> C {
+        self.collection
+    }
+}
+
+impl<S, C> NetworkAdapter for RLE<S, C>
+where
+    S: NetworkAdapter + ToPrimitive + FromPrimitive + Display,
+    C: IntoIterator + FromIterator<<C as IntoIterator>::Item> + 'static,
+    C::Item: NetworkAdapter,
+    C::IntoIter: ExactSizeIterator,
+{
+    fn get_be(bytes: &mut dyn Buf) -> Result<Self> {
+        // decode length
+        let len = S::get_be(bytes)?;
+
+        // attempt to convert length to usize
+        if let Some(len) = len.to_usize() {
+            // decode remaining items and convert to appropriate collection
+            (0..len)
+                .map(|_| C::Item::get_be(bytes))
+                .collect::<Result<C>>()
+                .map(|collection| Self {
+                    collection: collection,
+                    phantom: PhantomData::default(),
+                })
+        } else {
+            Err(err_msg(format!("cannot cast length to usize: {}", len)).into())
+        }
+    }
+
+    fn put_be(self, bytes: &mut dyn BufMut) -> Result<()> {
+        // convert collection to an iterator
+        let mut iter = self.collection.into_iter();
+
+        // attempt to convert length from a usize
+        if let Some(len) = S::from_usize(iter.len()) {
+            // encode length and then each element
+            len.put_be(bytes)?;
+            iter.try_for_each(|i| i.put_be(bytes))
+        } else {
+            Err(err_msg(format!("cannot cast length from usize: {}", iter.len())).into())
+        }
+    }
+}
+
+impl<S, C> Deref for RLE<S, C> {
+    type Target = C;
+
+    fn deref(&self) -> &Self::Target {
+        &self.collection
+    }
+}
+
+impl<S, C> Borrow<C> for RLE<S, C> {
+    fn borrow(&self) -> &C {
+        &self.collection
+    }
+}
+
+struct RLEString<S> {
+    string: String,
+    phantom: PhantomData<S>,
+}
+
+impl<S> RLEString<S> {
+    pub fn new(string: String) -> Self {
+        Self {
+            string,
+            phantom: PhantomData::default(),
+        }
+    }
+
+    pub fn unwrap(self) -> String {
+        self.string
+    }
+}
+
+// TODO: bug here? length in chars is used by game instead of length in bytes
+impl<S> NetworkAdapter for RLEString<S>
+where
+    S: NetworkAdapter + ToPrimitive + FromPrimitive + Display,
+{
+    fn get_be(bytes: &mut dyn Buf) -> Result<Self> {
+        let bytes = RLE::<S, Vec<u8>>::get_be(bytes)?.unwrap();
+        String::from_utf8(bytes)
+            .map(|string| Self {
+                string,
+                phantom: PhantomData::default(),
+            })
+            .map_err(|e| Error::Other(e.into()))
+    }
+
+    fn put_be(self, bytes: &mut dyn BufMut) -> Result<()> {
+        RLE::<S, _>::new(self.string.into_bytes()).put_be(bytes)
+    }
+}
+
+impl<S> Deref for RLEString<S> {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.string
+    }
+}
+
+impl<S> Borrow<String> for RLEString<S> {
+    fn borrow(&self) -> &String {
+        &self.string
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_matches::assert_matches;
+    use std::io::Cursor;
+    use std::mem::size_of;
+
+    #[test]
+    fn check_rle_size() {
+        assert_eq!(size_of::<RLE<u16, Vec<u32>>>(), size_of::<Vec<u32>>());
+    }
+
+    #[test]
+    fn test_rle_vec() {
+        let mut buf = vec![];
+        RLE::<u16, Vec<u8>>::new(vec![1, 2, 3, 4, 5])
+            .put_be(&mut buf)
+            .expect("encoding error");
+        assert_eq!(buf, vec![0, 5, 1, 2, 3, 4, 5]);
+
+        let output = RLE::<u16, Vec<u8>>::get_be(&mut Cursor::new(&buf)).expect("decoding error");
+        assert_eq!(output.unwrap(), vec![1, 2, 3, 4, 5]);
+
+        let large = (0..300).collect::<Vec<u16>>();
+        assert_matches!(
+            RLE::<u8, _>::new(large).put_be(&mut buf),
+            Err(Error::Other(_))
+        );
+    }
+
+    #[test]
+    fn test_rle_string() {
+        let mut buf = vec![];
+        RLEString::<u16>::new("hello world".to_owned())
+            .put_be(&mut buf)
+            .expect("encoding error");
+
+        let mut expected_encoded = {
+            let mut b = vec![0, 11];
+            b.extend_from_slice(b"hello world");
+            b
+        };
+
+        assert_eq!(buf, expected_encoded);
+
+        let output = RLEString::<u16>::get_be(&mut Cursor::new(&buf)).expect("decoding error");
+        assert_eq!(output.unwrap(), "hello world");
+
+        let large = "abc".repeat(100);
+        assert_matches!(
+            RLEString::<u8>::new(large).put_be(&mut buf),
+            Err(Error::Other(_))
+        )
+    }
+}
